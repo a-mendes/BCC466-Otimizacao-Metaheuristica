3cd1e6dcc61ce01f08b27a798cfb16968e349d38
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2024-07-24 15:33:22 -0300

    feat: construtivos e buscas locais

--- src/Solucao.cpp
+++ src/Solucao.cpp
@@ -5,7 +5,9 @@ void impSol(Solucao& sol)
     cout << "[ ";
     for(int i = 0 ; i < sol.size(); i ++)
     {
-        cout<< sol.at(i)<< " , ";
+        cout<< sol.at(i)<< " ";
+        if( i < (sol.size() -1) )
+            cout<< ", ";
     }
-    cout<< " 0 ]";
+    cout<< " ]";
 }
